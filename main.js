ac49e0a1a0b5db5112854178e901e3eb8df0ada5
Phil <phil@decoded.com>
2018-08-06 09:36:04 +0100

    move from messages to storage, allows us to store state]

--- main.js
+++ main.js
@@ -1,59 +1,73 @@
+// iife — Immediately-invoked function expression
+// read: it'll work itself on page load
+
 (function() {
   "use strict";
 
-  chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    console.log(request.enabled);
-    setEnabled(request.enabled);
+  // cleanAsana by default
+  setEnabled(true);
+
+  // when we notice a change in chrome storage
+  chrome.storage.onChanged.addListener(function(changes) {
+    // set cleanasana to the new value (true/false)
+    setEnabled(changes.enabled.newValue);
   });
 
   function cleanAsana() {
+
+    // make sure we've set the local store to true
+    chrome.storage.sync.set({enabled: true});
+
+    // change the background image
     var backgroundImage =
       "https://images.pexels.com/photos/991438/pexels-photo-991438.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260";
 
+    // create a new style sheet
     var sheet = document.createElement("style");
+
+    // give it an id (so we can delete it later)
     sheet.id = "cleanstylesheet";
 
+    // close the sidebar by default, show the sidebar button
     var sidebar = document.getElementsByClassName('sidebar-mountNode')
     sidebar[0].classList.add('is-collapsed');
-
     var sidebarButton = document.getElementsByClassName('ExpandSidebarButton')
     sidebarButton[0].classList.remove('ExpandSidebarButton--shouldHide')
 
+    // add these styles to the stylesheet
     sheet.innerHTML = `
+      /* change font colour */
+      .TopbarPageHeaderStructure-title, .BoardColumnHeader, .ProjectPageHeader-projectName, .ClarifiedNavigationHeaderView-title{
+        color:white;
+      }
 
-         /* change font colour */
-         .TopbarPageHeaderStructure-title, .BoardColumnHeader, .ProjectPageHeader-projectName, .ClarifiedNavigationHeaderView-title{
-            color:white;
-         }
+      /* background image */
+      #asana_main{
+         background-image:url( ${backgroundImage} );
+         background-size: cover;
+      }
 
-         /* background image */
-         #asana_main{
-             background-image:url( ${backgroundImage} );
-             background-size: cover;
-         }
+      /* hide ALL of this */
+      .SidebarTeamMembersList, .ClarifiedNavigationHeaderView-titleRightElement, .team-feed, .teamFeedView-emptyStateCard, .team-page-description, .messageComposerView-previewView, .Facepile .SidebarTopNavLinks-myTasksButton, .SidebarTopNavLinks-notificationsButton, .SidebarTopNavLinks-notificationsButton, .SidebarTopNavLinks-myDashboardButton, .SidebarRecentsAndFavorites, .SidebarReports, .BoardBody-descriptionLink, .FloatingSelect, .pageTopbarView-globalActions, .Tab:nth-last-child(-n+4), .projectHeaderFacepile-content, .TopbarPageHeaderStructure-titleRightElement, .TaskStoryFeed, .CommentComposer-avatar, .FollowersBar, .HeartButton {
+        display:none;
+      }
 
-         /* hide ALL of this */
-         .SidebarTeamMembersList, .ClarifiedNavigationHeaderView-titleRightElement, .team-feed, .teamFeedView-emptyStateCard, .team-page-description, .messageComposerView-previewView, .Facepile .SidebarTopNavLinks-myTasksButton, .SidebarTopNavLinks-notificationsButton, .SidebarTopNavLinks-notificationsButton, .SidebarTopNavLinks-myDashboardButton, .SidebarRecentsAndFavorites, .SidebarReports, .BoardBody-descriptionLink, .FloatingSelect, .pageTopbarView-globalActions, .Tab:nth-last-child(-n+3), .projectHeaderFacepile-content, .TopbarPageHeaderStructure-titleRightElement {
-            display:none;
-         }
+      /* transparent navbar */
+      .page-topbar{
+        background:rgba(255,255,255,0);
+      }`;
 
-         /* transparent navbar */
-         .page-topbar{
-            background:rgba(255,255,255,0);
-         }`;
-
+    // add the stylesheet to the page
     document.body.appendChild(sheet);
   }
+
+  // choose whether to clean asana or not
   function setEnabled(enabled) {
     if (enabled) {
       cleanAsana();
-      console.log("tidy");
     } else {
       var stylesheet = document.getElementById("cleanstylesheet");
       document.body.removeChild(stylesheet);
-      console.log("remove");
     }
   }
-
-
 })();
--- popup.js
+++ popup.js
@@ -1,6 +1,25 @@
-document.getElementById('toggle').addEventListener('click', function(e){
-  var enabled = e.target.checked
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {enabled: enabled});
-  });
+
+// see if it's enabled from local storage
+chrome.storage.sync.get('enabled', function(data){
+  // if not set yet
+  if(chrome.runtime.lastError){
+    // set it to true
+    chrome.storage.sync.set({enabled: true});
+    return;
+  } else{
+    // if it is set, check/uncheck the checkbox accordingly
+    document.getElementById('toggle').checked = data.enabled;
+    switchEnabled();
+  }
 })
+
+function switchEnabled(){
+  // get toggle, wait til it's clicked
+  document.getElementById('toggle').addEventListener('click', function(e){
+    // set enabled to true/false
+    var enabled = e.target.checked
+    console.log(enabled)
+    // sync that value with local storage
+    chrome.storage.sync.set({enabled: enabled});
+  })
+}
